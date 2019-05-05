56e5f855657bb6790e6dab13bfaf27533f5dfe2b
Andrew Hercules <andrewhercules@gmail.com>
2019-05-05 13:52:17 +0100

    Added template for targets array

--- js/application.js
+++ js/application.js
@@ -1,3 +1,15 @@
+targets = [
+  {
+    "ensembl_id": "",
+    "uniprot_id": "",
+    "hgnc_symbol": "",
+    "description": "",
+    "therapeutic_area": "",
+    "colour": "",
+  },
+]
+
 $( document ).ready(function() {
   console.log( "ready!" );
+  console.log(targets[0])
 });
