debdacbc6f988350eac566fd7e63813f376fde5f
Andrew Hercules <andrewhercules@gmail.com>
2019-05-05 15:07:58 +0100

    Added section for citation

--- js/application.js
+++ js/application.js
@@ -25,6 +25,21 @@ targets = [
   },
 ]
 
+citations = [
+  {
+    "style": "APA",
+    "citation": "Hercules, A. (2019). Trendy Tools: Colour Theory meets Genetics. Nature, 186(5), 36-42. doi: 10.1025/nat-030291929882."
+  },
+  {
+    "style": "Chicago",
+    "citation": 'Hercules, Andrew. "Trendy Tools: Colour Theory meets Genetics." Nature 186, no. 5 (2019). doi: 10.1025/nat-030291929882'
+  },
+  {
+    "style": "MLA",
+    "citation": 'Hercules, Andrew. "Trendy Tools: Colour Theory meets Genetics." Nature, vol 186, no. 5, 2019, pp. 36-42.'
+   }
+];
+
 $( document ).ready(function() {
   console.log( "ready!" );
   console.log(targets[0])
@@ -40,11 +55,24 @@ $('#run-pipeline-btn').on("click", function(){
     $("body").addClass("finished-loading"); 
     $('#run-pipeline-btn').hide();
     selectTarget()
-  }, 3000);
-  
+  }, 500);
+
+});
+
 function selectTarget() {
   var randomTarget = targets[Math.floor(Math.random() * targets.length)];
   $('#target-name').text(randomTarget.hgnc_symbol);
 }
-  
+
+// Show spinner for 3 seconds, then return to original screen
+$('#rerun-pipeline-btn').on("click", function(){
+  $('#pipeline-results').hide();
+  $("body").addClass("loading");
+  setTimeout(function(){ 
+    $("body").removeClass("loading");
+    $("body").addClass("finished-loading");
+    $('#pipeline-results').show(); 
+    $('#run-pipeline-btn').hide();
+    selectTarget()
+  }, 500);
 });
