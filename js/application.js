78b95b3f15ead0e78e8ecbb90916d276c0e3c262
Andrew Hercules <andrewhercules@gmail.com>
2019-05-05 14:21:50 +0100

    Added functionality that results block shows after pipeline finished processing

--- js/application.js
+++ js/application.js
@@ -13,3 +13,18 @@ $( document ).ready(function() {
   console.log( "ready!" );
   console.log(targets[0])
 });
+
+
+
+// Show spinner for 3 seconds, then return to original screen
+$('#run-pipeline-btn').on("click", function(){
+  $("body").addClass("loading");
+  setTimeout(function(){ 
+    $("body").removeClass("loading");
+    $("body").addClass("finished-loading"); 
+    $('#run-pipeline-btn').hide();
+    $('#target-name').text('ESR1');
+  }, 3000);
+  
+  
+});
