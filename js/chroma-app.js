4e42d2326f6fe26e8a6853151c901453ad5aa5f7
Andrew Hercules <andrewhercules@gmail.com>
2019-09-08 15:50:54 +0100

    Updated CHROMA app JS logic

--- js/chroma-app.js
+++ js/chroma-app.js
@@ -8,58 +8,50 @@ $(document).ready(function () {
     console.log('hovering on colour theory definition tooltip')
   });
 
-  // After user has answered two questions, run pipeline to generate and display response after 3 seconds
-  $('#run-pipeline-btn').on("click", function (event) {
-    $('#run-pipeline-block').hide();
-    startPipeline(event);
-    setTimeout(function () {
-      chromaPipeline();
-    }, 3000);
-  });
-
-  // After user has run pipeline for the first time, if they select 'Re-run pipeline', run the same pipeline 
-  $('#rerun-pipeline-btn').on("click", function (event) {
-    $('#pipeline-results').hide();
-    startPipeline(event);
-    setTimeout(function () {
-      chromaPipeline();
-    }, 3000);
-  });
-
-  // Initialise and start CHROMA pipeline
-  function startPipeline(event) {
+  // After user has answered two questions, initalise and run pipeline, and display response after 3 seconds
+  $('#run-pipeline-btn').on('click', function (event) {
     event.preventDefault();
-    $("body").addClass("loading");
-    console.log('pipeline started')
-  }
+    console.log('CHROMA pipeline has started')
+    $('#run-pipeline-block').hide();
+    $('body').addClass('loading');
+    console.log('CHROMA pipeline is running')
+    setTimeout(function () {
+      runCHROMAPipeline();
+    }, 3000);
+  });
 
   // run CHROMA pipeline
-  function chromaPipeline() {
-    $("body").removeClass("loading");
-    $("body").addClass("finished-loading");
+  function runCHROMAPipeline() {
+    $('body').removeClass('loading');
+    $('body').addClass('finished-loading');
     $('#run-pipeline-btn').hide();
-    selectGene()
-    console.log('pipeline finished')
+    randomGene = selectGene();
+    updateUI(randomGene);
     $('#pipeline-results').show();
+    console.log('CHROMA pipeline has finished')
   }
 
-  // Using array found in genes.js file, select a random gene to mock pipeline process and update UI with results
+  // Using array found in genes.js file, select a random gene to mock pipeline process, and update UI with results
   function selectGene() {
-    var randomGene = genes[Math.floor(Math.random() * genes.length)];
+    return genes[Math.floor(Math.random() * genes.length)];
+  }
+
+  function updateUI(randomGene) {
     $('#gene-name').text(randomGene.hgncSymbol);
     $('#gene-description').text(randomGene.description);
     $('#gene-colour-profile').text(randomGene.pipelineColour);
     $('#gene-pipeline-assessment').text(randomGene.pipelineAssessment)
-    $('#gene-open-targets-link').attr("href", "https://www.targetvalidation.org/target/" + randomGene.ensemblId);
-    $('#gene-ensembl-link').attr("href", "https://www.ensembl.org/Homo_sapiens/Gene/Summary?g=" + randomGene.ensemblId);
-    $('#gene-uniprot-link').attr("href", "https://www.uniprot.org/uniprot/" + randomGene.uniprotId);
-    $('#gene-genecards-link').attr("href", "https://www.genecards.org/cgi-bin/carddisp.pl?gene=" + randomGene.hgncId);
-    $('#gene-gnomad-link').attr("href", "https://gnomad.broadinstitute.org/gene/" + randomGene.hgncId);
+    $('#gene-open-targets-link').attr('href', 'https://www.targetvalidation.org/target/' + randomGene.ensemblId);
+    $('#gene-ensembl-link').attr('href', 'https://www.ensembl.org/Homo_sapiens/Gene/Summary?g=' + randomGene.ensemblId);
+    $('#gene-uniprot-link').attr('href', 'https://www.uniprot.org/uniprot/' + randomGene.uniprotId);
+    $('#gene-genecards-link').attr('href', 'https://www.genecards.org/cgi-bin/carddisp.pl?gene=' + randomGene.hgncId);
+    $('#gene-gnomad-link').attr('href', 'https://gnomad.broadinstitute.org/gene/' + randomGene.hgncId);  
   }
-
+  
   // If user clicks 'Start over', hide section with pipeline results and display form to run pipeline
   $('#clear-pipeline-results-btn').on('click', function (event) {
     event.preventDefault();
+    $('#run-pipeline-form').trigger('reset');
     $('#pipeline-results').hide();
     $('#run-pipeline-btn').show();
     $('#run-pipeline-block').show();
@@ -71,11 +63,12 @@ $(document).ready(function () {
   });
 
   // When user clicks on 'Get citation' button, get the relevant citation and display
-  $('#get-citation-btn').on("click", function (event) {
+  $('#get-citation-btn').on('click', function (event) {
     event.preventDefault();
     $('body').addClass('citation-style-selected');
     var selectedCitationStyle = $('#selectCitationStyle').val()
     var citationText = citations[selectedCitationStyle];
+    console.log('Showing ' + selectedCitationStyle + ' citation' )
     $('#citation-style').text(selectedCitationStyle);
     $('#citation-text').text(citationText);
   });
